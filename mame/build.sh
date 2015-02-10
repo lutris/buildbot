1cdf2ef2922d8498189b51872e9cf0aa16f71a0b
Mathieu Comandon <strycore@gmail.com>
2015-02-10 15:44:10 +0100

    Add mame/mess build script

---
+++ mame/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get install -y curl wget unzip debhelper \
+    libexpat1-dev libflac-dev libfontconfig1-dev \
+    libjpeg8-dev libportmidi-dev libqt4-dev libsdl-ttf2.0-dev \
+    libsdl2-dev libxinerama-dev subversion python-dev zlib1g-dev 
+release=$(curl http://mamedev.org/release.html | grep "href.*s.zip" | cut -d"\"" -f 2)
+archive=$(echo ${release} | cut -d"/" -f 2)
+wget "http://mamedev.org/${release}" -O ${archive}
+unzip -o $archive
+unzip -o mame.zip
+
+unset FULLNAME
+make
+TARGET=mess make
