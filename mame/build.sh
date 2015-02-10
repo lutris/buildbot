ac32364a853c2520369664610578d53e68971dad
Mathieu Comandon <strycore@gmail.com>
2015-02-10 17:08:13 +0100

    Fix mame dependency

--- mame/build.sh
+++ mame/build.sh
@@ -2,8 +2,8 @@
 
 apt-get install -y curl wget unzip debhelper \
     libexpat1-dev libflac-dev libfontconfig1-dev \
-    libjpeg8-dev libportmidi-dev libqt4-dev libsdl-ttf2.0-dev \
-    libsdl2-dev libxinerama-dev subversion python-dev zlib1g-dev 
+    libjpeg8-dev libportmidi-dev libqt4-dev libsdl2-ttf-dev \
+    libsdl2-dev libxinerama-dev subversion python-dev zlib1g-dev
 release=$(curl http://mamedev.org/release.html | grep "href.*s.zip" | cut -d"\"" -f 2)
 archive=$(echo ${release} | cut -d"/" -f 2)
 wget "http://mamedev.org/${release}" -O ${archive}
