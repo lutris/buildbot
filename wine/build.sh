2ea7eca97e601443040a202495770c7ab39c81e8
Mathieu Comandon <strycore@gmail.com>
2015-03-05 13:40:40 +0100

    Add wine build script

---
+++ wine/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sourcedir="$HOME/wine-git"
+builddir="$HOME/wine32"
+installdir="/opt"
+version="1.7.37"
+arch="i386"
+
+destdir="${version}-${arch}"
+
+rm -rf "$builddir"
+mkdir $builddir
+cd $builddir
+$sourcedir/configure --prefix=${installdir}/${destdir}
+make -j4
+make install
+
+cd $installdir
+find . -type f -exec strip {} \;
+tar cvzf wine-${version}-${arch}.tar.gz ${destdir}
