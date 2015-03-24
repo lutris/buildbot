fe6546e1f1e6658b5c912424d28a55048d9f6227
Mathieu Comandon <strycore@gmail.com>
2015-03-25 00:22:45 +0100

    Fix wine paths

--- runners/wine/build.sh
+++ runners/wine/build.sh
@@ -3,30 +3,30 @@
 set -e
 
 root_dir=$(pwd)
-sourcedir="${root_dir}/wine-src"
-builddir="${root_dir}/wine"
+source_dir="${root_dir}/wine-src"
+build_dir="${root_dir}/wine"
 version="1.7.39"
 arch=$(uname -m)
 
-destdir="${version}-${arch}"
+dest_dir="${version}-${arch}"
 
-if [ -d ${sourcedir} ]; then
-    cd ${sourcedir}
+if [ -d ${source_dir} ]; then
+    cd ${source_dir}
     git checkout master
     git pull
 else
-    git clone git://source.winehq.org/git/wine.git $sourcedir
-    cd $sourcedir
+    git clone git://source.winehq.org/git/wine.git $source_dir
+    cd $source_dir
 fi
 git checkout -b wine-${version}
 
-mkdir $builddir
-cd $builddir
-$sourcedir/configure --prefix=${installdir}/${destdir}
+mkdir -p $build_dir
+cd $build_dir
+$source_dir/configure --prefix=${root_dir}/${dest_dir}
 make -j4
 make install
 
 cd ${root_dir}
 find . -type f -exec strip {} \;
-tar cvzf wine-${version}-${arch}.tar.gz ${destdir}
-rm -rf ${builddir} ${source_dir}
+tar cvzf wine-${version}-${arch}.tar.gz ${dest_dir}
+rm -rf ${build_dir} ${source_dir}
