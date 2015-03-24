478e572931efbe6740fcaf2bf9c4fe94831d2ff5
Mathieu Comandon <strycore@gmail.com>
2015-03-25 00:15:15 +0100

    Enhance wine build script

--- runners/wine/build.sh
+++ runners/wine/build.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-rootdir=$(pwd)
-sourcedir="${rootdir}/wine-src"
-builddir="${rootdir}/wine"
-installdir="/opt"
+set -e
+
+root_dir=$(pwd)
+sourcedir="${root_dir}/wine-src"
+builddir="${root_dir}/wine"
 version="1.7.39"
 arch=$(uname -m)
 
 destdir="${version}-${arch}"
 
-cd $sourcedir
-git co master
-git pull
-git co -b wine-${version}
+if [ -d ${sourcedir} ]; then
+    cd ${sourcedir}
+    git checkout master
+    git pull
+else
+    git clone git://source.winehq.org/git/wine.git $sourcedir
+    cd $sourcedir
+fi
+git checkout -b wine-${version}
 
-rm -rf "$builddir"
 mkdir $builddir
 cd $builddir
 $sourcedir/configure --prefix=${installdir}/${destdir}
 make -j4
 make install
 
-cd $installdir
+cd ${root_dir}
 find . -type f -exec strip {} \;
 tar cvzf wine-${version}-${arch}.tar.gz ${destdir}
+rm -rf ${builddir} ${source_dir}
