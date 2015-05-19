852f67c0f86f48dd00d1779e4de2599ab3b28161
Mathieu Comandon <strycore@gmail.com>
2015-05-18 17:21:21 -0700

    Complete build script for reicast

--- runners/reicast/build.sh
+++ runners/reicast/build.sh
@@ -3,12 +3,27 @@
 set -e
 
 pkg_name="reicast"
-version="0.0.1"
-arch=$(uname -a)
+version="r7"
+arch=$(uname -m)
 
 root_dir="$(pwd)"
 source_dir="${root_dir}/${pkg_name}-src"
 build_dir="${root_dir}/${pkg_name}"
 
+sudo apt-get install libegl1-mesa-dev libgles2-mesa-dev libasound2-dev mesa-common-dev freeglut3-dev
+
 git clone https://github.com/reicast/reicast-emulator.git ${source_dir}
 
+if [ "${arch}" == "i686" ]; then
+    git checkout origin/skmp/linux86 -b linux86
+else
+    git checkout origin/skmp/linux-x64 -b linux-x64
+fi
+
+cd ${source_dir}/shell/lin86
+make
+
+mkdir -p ${build_dir}
+mv reicast.elf nosym-reicast.elf ${build_dir}
+cd ${root_dir}
+tar czf ${pkg_name}-${version}-${arch}.tar.gz ${pkg_name}
