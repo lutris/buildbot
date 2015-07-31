b1407c0a5bbaad3f96cefd6fe23227a577335aff
Mathieu Comandon <strycore@gmail.com>
2015-07-31 15:52:55 -0700

    Change filename if bulding wine staging

--- runners/wine/build.sh
+++ runners/wine/build.sh
@@ -13,25 +13,31 @@ build_dir="${root_dir}/wine"
 version="1.7.48"
 arch=$(uname -m)
 
-dest_dir="${version}-${arch}"
 
 if [ -d ${source_dir} ]; then
+    echo "Updating sources"
     cd ${source_dir}
     git checkout master
     git pull
 else
+    echo "Cloning sources"
     git clone git://source.winehq.org/git/wine.git $source_dir
     cd $source_dir
 fi
+
+echo "Checking out wine ${version}"
 git checkout wine-${version}
 
 if [ $STAGING ]; then
+    echo "Adding Wine Staging patches"
     wget https://github.com/wine-compholio/wine-staging/archive/v${version}.tar.gz
     tar xvzf v${version}.tar.gz --strip-components 1
     ./patches/patchinstall.sh DESTDIR="$(pwd)" --all
     configure_opts="--with-xattr"
+    filename_opts="staging-"
 fi
 
+dest_dir="${filename_opts}${version}-${arch}"
 mkdir -p $build_dir
 cd $build_dir
 $source_dir/configure ${configure_opts} --prefix=${root_dir}/${dest_dir}
@@ -40,5 +46,5 @@ make install
 
 cd ${root_dir}
 find . -type f -exec strip {} \;
-tar cvzf wine-${version}-${arch}.tar.gz ${dest_dir}
+tar cvzf wine-{$filename_opts}${version}-${arch}.tar.gz ${dest_dir}
 rm -rf ${build_dir} ${source_dir}
