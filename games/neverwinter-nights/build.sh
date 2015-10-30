c03dddb452f1510d6fe326e8385cd9f19e739eaa
Mathieu Comandon <strycore@gmail.com>
2015-10-29 17:00:24 -0700

    Complete nwn libs script

--- games/neverwinter-nights/build.sh
+++ games/neverwinter-nights/build.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-git clone https://github.com/nwnlinux/nwlogger.git
+sudo apt-get install elfutils libelf-dev linux-headers-generic linux-libc-dev
 
+mkdir -p lib
+
+git clone https://github.com/nwnlinux/nwlogger.git
 cd nwlogger
 ./nwlogger_install.pl
+cd ..
+
+git clone https://github.com/nwnlinux/nwmovies.git
+cd nwmovies
+./nwmovies_install.pl
+cd ..
+
+git clone https://github.com/nwnlinux/nwuser.git
+cd nwuser
+./nwuser_install.pl
+cd ..
+
+git clone https://github.com/nwnlinux/nwmouse.git
+cd nwmouse
+./nwmouse_install.pl
