663318778f618a3c94d447a7c7ace139a8e69763
Mathieu Comandon <strycore@gmail.com>
2015-10-29 17:11:10 -0700

    Copy compiled nwn libs to dest folder

--- games/neverwinter-nights/build.sh
+++ games/neverwinter-nights/build.sh
@@ -6,19 +6,24 @@ mkdir -p lib
 
 git clone https://github.com/nwnlinux/nwlogger.git
 cd nwlogger
+sed -i -e "s%linux/user.h%sys/user.h%" nwlogger/nwlogger_cookie.c
 ./nwlogger_install.pl
+cp nwlogger/nwlogger.so ../lib
 cd ..
 
 git clone https://github.com/nwnlinux/nwmovies.git
 cd nwmovies
 ./nwmovies_install.pl
+cp nwmovies/binklib.so nwmovies/nwmovies.so ../lib
 cd ..
 
 git clone https://github.com/nwnlinux/nwuser.git
 cd nwuser
 ./nwuser_install.pl
+cp nwuser/nwuser.so ../lib
 cd ..
 
 git clone https://github.com/nwnlinux/nwmouse.git
 cd nwmouse
+cp mwmouse/nwmouse.so ../lib
 ./nwmouse_install.pl
