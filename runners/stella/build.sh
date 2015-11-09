8674cc8e32cfac41cdae0fc5db67a9c5468bc13c
Mathieu Comandon <strycore@gmail.com>
2015-11-09 13:19:42 -0800

    Add stella build script

--- runners/stella/build.sh
+++ runners/stella/build.sh
@@ -6,29 +6,24 @@ source ${lib_path}path.sh
 source ${lib_path}util.sh
 source ${lib_path}upload_handler.sh
 
-deps="libncurses5-dev"
-
 runner_name=$(get_runner)
 root_dir=$(pwd)
 bin_dir="${root_dir}/${runner_name}"
 arch=$(uname -m)
-version="2.44"
+version="4.6.7"
 
 src_dir="${runner_name}-${version}"
-src_archive="${src_dir}.tar.gz"
-src_url="http://www.ifarchive.org/if-archive/infocom/interpreters/frotz/${src_archive}"
-
-install_deps $deps
-
-wget $src_url
-tar xzf $src_archive
+src_archive="${src_dir}-src.tar.gz"
+src_url="http://prdownloads.sourceforge.net/stella/${src_archive}?download"
+wget ${src_url} -O $src_archive
+tar xzf ${src_archive}
 cd ${src_dir}
+
+./configure --prefix=${bin_dir}
 make
+make install
 
-mkdir -p ${bin_dir}
-cp frotz AUTHORS BUGS COPYING HOW_TO_PLAY README ${bin_dir} 
 cd ..
-
 dest_file="${runner_name}-${version}-${arch}.tar.gz"
 tar czf ${dest_file} ${runner_name}
 runner_upload ${runner_name} ${version} ${arch} ${dest_file}
