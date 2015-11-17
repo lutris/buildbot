1be478afb63b5492f2d612b4f919c7d2c9950bc9
Mathieu Comandon <strycore@gmail.com>
2015-11-17 09:35:45 -0800

    Better do it in the steam-runtime folder

--- runtime/build.sh
+++ runtime/build.sh
@@ -15,11 +15,12 @@ runtime_dir="lib${bit}"
 # Steam runtime
 # Only build steam runtime once since it contains both archs
 if [ "$STEAM" = '1' ]; then
-    # Remove old runtime build
-    rm -rf steam
 
     steam_runtime_file="steam.tar.bz2"
     cd steam-runtime
+    # Remove old runtime build
+    rm -rf steam
+
     python2 build-runtime.py
     mv runtime steam
     cd steam
