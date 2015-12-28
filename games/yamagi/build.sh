376871467c7fbe2dcc0378b5ec927435e40fa5d1
Mathieu Comandon <strycore@gmail.com>
2015-12-28 12:29:04 -0800

    Add yamagi build script

---
+++ games/yamagi/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+lib_path="../../lib/"
+
+source ${lib_path}path.sh
+source ${lib_path}util.sh
+source ${lib_path}upload_handler.sh
+
+root_dir=$(pwd)
+package_name=yamagi
+version=5.33
+arch=$(uname -m)
+game=yquake2
+
+params=$(getopt -n $0 -o rx --long rogue,xatrix -- "$@")
+eval set -- $params
+while true ; do
+    case "$1" in
+        -r|--rogue) game=rogue; shift ;;
+        -x|--xatrix) game=xatrix; shift ;;
+        *) shift; break ;;
+    esac
+done
+
+source_dir=${root_dir}/${package_name}-${game}-src
+clone https://github.com/yquake2/${game}.git ${source_dir}
+
+cd $source_dir
+make -j$(getconf _NPROCESSORS_ONLN)
+cp -a release ../${game}
+cd ..
+
+tar czf ${package_name}-${game}-${version}-${arch}.tar.gz ${game}
