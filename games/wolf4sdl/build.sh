05a33f4cf29ffa465010f176225d0cd531869fcc
Mathieu Comandon <strycore@gmail.com>
2015-12-28 04:41:10 -0800

    Add spear of destiny support

--- games/wolf4sdl/build.sh
+++ games/wolf4sdl/build.sh
@@ -11,12 +11,30 @@ root_dir=$(pwd)
 version=1.7
 arch=$(uname -m)
 source_dir=${root_dir}/wolf4sdl-src
+execname=wolf3d
+
+params=$(getopt -n $0 -o s --long spear -- "$@")
+eval set -- $params
+while true ; do
+    case "$1" in
+        -s|--spear) spear=1; shift ;;
+        *) shift; break ;;
+    esac
+done
 
 clone https://github.com/ljbade/wolf4sdl.git $source_dir
 cd $source_dir
 
-DATADIR= make
+if [ "$spear" ]; then
+    sed -i '/define SPEAR$/s/\/\///' version.h
+    execname=spear
+fi
 
-tar czf ../wolf4sdl-${version}-${arch}.tar.gz wolf3d
+DATADIR= make
+if [ $execname != 'wolf3d' ]; then
+    mv wolf3d $execname
+fi
+
+tar czf ../wolf4sdl-${execname}-${version}-${arch}.tar.gz $execname
 cd ..
 rm -rf $source_dir
