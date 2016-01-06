c7cd2745b43ac056ede90caf0143cdc0a24f9435
Mathieu Comandon <strycore@gmail.com>
2016-01-06 15:04:37 -0800

    Add PPSSPP build script

---
+++ runners/ppsspp/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+lib_path="../../lib/"
+source ${lib_path}path.sh
+source ${lib_path}util.sh
+source ${lib_path}upload_handler.sh
+
+runner_name=$(get_runner)
+root_dir=$(pwd)
+source_dir="${root_dir}/${runner_name}-src"
+build_dir="${root_dir}/${runner_name}-build"
+bin_dir="${root_dir}/${runner_name}"
+arch=$(uname -m)
+version="1.1.1"
+
+deps=""
+
+clone https://github.com/hrydgard/ppsspp.git $source_dir
+cd $source_dir
+git submodule update --init --recursive
+
+mkdir -p $build_dir
+cd $build_dir
+cmake ${source_dir}
+make -j$(getconf _NPROCESSORS_ONLN)
+
+cd ..
+mkdir -p ${bin_dir}
+cp -a ${build_dir}/assets ${bin_dir}
+cp ${build_dir}/PPSSPPSDL ${bin_dir}
+
+dest_file="${runner_name}-${version}-${arch}.tar.gz"
+tar czf ${dest_file} ${runner_name}
+rm -rf ${build_dir} ${source_dir} ${bin_dir}
+
+runner_upload ${runner_name} ${version} ${arch} ${dest_file}
