40b86778e3e3e91d988aac82b5eca0dd290532a0
Mathieu Comandon <strycore@gmail.com>
2016-01-27 15:02:02 -0800

    Complete OpenTomb script (current build is broken)

--- games/opentomb/build.sh
+++ games/opentomb/build.sh
@@ -7,9 +7,18 @@ source ../../lib/util.sh
 version="master"
 root_dir=$(pwd)
 source_dir="${root_dir}/opentomb-src"
+build_dir="${root_dir}/opentomb-build"
 
-clone https://github.com/opentomb/OpenTomb.git $source_dir
+install_deps liblua5.3-dev \
+    libbullet-dev libfreetype6-dev libglu1-mesa-dev libglew-dev \
+    libopenal-dev libogg-dev libvorbis-dev libsndfile1-dev libsdl2-dev \
+    libsdl2-image-dev
+
+# git clone --recursive https://github.com/opentomb/OpenTomb.git $source_dir
+git clone --recursive https://github.com/stohrendorf/OpenTomb.git $source_dir
+#mkdir -p $build_dir
+#cd $build_dir
 
 cd $source_dir
-
-
+cmake .
+make
