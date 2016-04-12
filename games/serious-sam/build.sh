ce9da1d55a3d2e81f702a9583da136d3b62b13df
Mathieu Comandon <strycore@gmail.com>
2016-04-12 00:12:26 -0700

    Finish Serious Engine build script

--- games/serious-sam/build.sh
+++ games/serious-sam/build.sh
@@ -18,3 +18,13 @@ clone https://github.com/rcgordon/Serious-Engine.git $source_dir
 cd $source_dir/Sources
 ./build-linux.sh
 
+mkdir -p $bin_dir
+mkdir -p $bin_dir/Debug
+
+cd cmake-build
+mv ecc ssam libShadersD.so $bin_dir
+mv libEntitiesMPD.so $bin_dir/Debug/libEntitiesD.so
+mv libGameMPD.so $bin_dir/Debug/libGameD.so
+
+cd root_dir
+tar cJf ${package_name}-${arch}.tar.bz2 ${package_name}
