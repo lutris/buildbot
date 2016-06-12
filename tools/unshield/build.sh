e097573a263a7ebeb1031fbe8da8e30744022e52
Mathieu Comandon <strycore@gmail.com>
2016-06-11 18:08:55 -0700

    Add unshield script

---
+++ tools/unshield/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+source ../../lib/util.sh
+
+pkg_name="unshield"
+version="1.3"
+arch=$(uname -m)
+root_dir=$(pwd)
+source_dir="${root_dir}/${pkg_name}-src"
+build_dir="${root_dir}/${pkg_name}-build"
+bin_dir="${root_dir}/${pkg_name}"
+
+
+GetSources() {
+    clone https://github.com/twogood/unshield.git $source_dir
+}
+
+BuildProject() {
+    mkdir -p $build_dir
+    cd $build_dir
+    cmake $source_dir
+    make
+}
+
+PackageProject() {
+    mkdir -p $bin_dir
+    mv ${build_dir}/lib/libunshield* $bin_dir
+    mv ${build_dir}/src/unshield* $bin_dir
+    cd $root_dir
+    tar czf ${pkg_name}-${version}-${arch}.tar.gz ${pkg_name}
+}
+
+Cleanup() {
+    cd $root_dir
+    rm -rf $bin_dir
+    rm -rf $build_dir
+    rm -rf $source_dir
+}
+
+
+if [ $1 ]; then
+    $1
+else
+    GetSources $version
+    BuildProject
+    PackageProject
+    Cleanup
+fi
