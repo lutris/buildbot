576c2050abdea47abc62dadb66b08a1d62e69b9b
Rob Loach <robloach@gmail.com>
2016-09-25 16:53:57 -0400

    Update to SuperTux 0.5.0

--- lib/util.sh
+++ lib/util.sh
@@ -2,6 +2,7 @@ function clone {
     repo_url=$1
     source_dir=$2
     recurse=$3
+    tag=$4
 
     if [ "$recurse" ]; then
         recurse="--recursive"
@@ -17,6 +18,9 @@ function clone {
         echo "Cloning sources"
         git clone ${recurse} ${repo_url} ${source_dir}
         cd $source_dir
+    fi
+    if [ "$tag" ]; then
+        git checkout ${tag}
     fi
 }
 
