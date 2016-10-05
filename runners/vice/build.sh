3b9a58a43429d6b57453b2a68a22c1943a250b40
Mathieu Comandon <strycore@gmail.com>
2016-10-05 15:56:56 -0700

    [VICE] Modularize build script

--- runners/vice/build.sh
+++ runners/vice/build.sh
@@ -12,23 +12,40 @@ bin_dir="${root_dir}/${runner_name}"
 arch=$(uname -m)
 version="2.4"
 
-deps="libxxf86vm-dev libxmu-dev libxaw7-dev libreadline-dev"
-install_deps $deps
+InstallBuildDependencies() {
+    deps="libxxf86vm-dev libxmu-dev libxaw7-dev libreadline-dev"
+    install_deps $deps
+}
 
-src_dir="${runner_name}-${version}"
-src_archive="${src_dir}.tar.gz"
-src_url="http://sourceforge.net/projects/vice-emu/files/releases/${src_archive}/download"
+GetSources() {
+    src_dir="${runner_name}-${version}"
+    src_archive="${src_dir}.tar.gz"
+    src_url="http://sourceforge.net/projects/vice-emu/files/releases/${src_archive}/download"
 
-wget ${src_url} -O ${src_archive}
-tar xzf ${src_archive}
-cd ${src_dir}
+    wget ${src_url} -O ${src_archive}
+    tar xzf ${src_archive}
+}
 
-./configure --prefix=${bin_dir}
-make
-make install
+BuildProject() {
+    cd ${src_dir}
+    ./configure --prefix=${bin_dir}
+    make
+    make install
+}
 
-cd ..
-dest_file="${runner_name}-${version}-${arch}.tar.gz"
-tar czf ${dest_file} ${runner_name}
-runner_upload ${runner_name} ${version} ${arch} ${dest_file}
-rm -rf ${src_dir} ${src_archive} ${bin_dir}
+PackageProject() {
+    cd $root_dir
+    dest_file="${runner_name}-${version}-${arch}.tar.gz"
+    tar czf ${dest_file} ${runner_name}
+    runner_upload ${runner_name} ${version} ${arch} ${dest_file}
+    rm -rf ${src_dir} ${src_archive} ${bin_dir}
+}
+
+if [ $1 ]; then
+    $1
+else
+    InstallBuildDependencies
+    GetSources
+    BuildProject
+    PackageProject
+fi
