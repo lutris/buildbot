90cfaabea3ac65b39de921c340b81b491b27efa5
Mathieu Comandon <strycore@gmail.com>
2016-10-06 12:28:18 -0700

    Add build script for Wesnoth

---
+++ games/wesnoth/build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+source ../../lib/util.sh
+
+version="1.12.6"
+arch="$(uname -m)"
+root_dir=$(pwd)
+source_dir="${root_dir}/wesnoth-src"
+build_dir="${root_dir}/wesnoth-build"
+bin_dir="${root_dir}/wesnoth"
+
+InstallBuildDependencies() {
+    sudo apt-get build-dep wesnoth
+}
+
+GetSources() {
+    cd $root_dir
+    dest="wesnoth-${version}.tar.bz2"
+    if [ ! -f "$dest" ]; then
+        wget https://sourceforge.net/projects/wesnoth/files/wesnoth-1.12/wesnoth-${version}/${dest}/download -O $dest
+    fi
+    tar xvjf $dest
+    rm -rf $source_dir
+    mv wesnoth-${version} ${source_dir}
+}
+
+BuildProject() {
+    cd $root_dir
+    mkdir -p $build_dir
+    cd $build_dir
+    cmake $source_dir
+    make
+}
+
+PackageProject() {
+    cd $build_dir
+    mkdir -p $bin_dir
+    cp wesnoth wesnothd $bin_dir
+    cd $source_dir
+    cp -a sounds $bin_dir
+    cp -a misc $bin_dir
+    cp -a data $bin_dir
+    cp -a icons $bin_dir
+    cp -a images $bin_dir
+    cp -a fonts $bin_dir
+    tar czf wesnoth-${version}-${arch}-tar.gz
+}
+
+Cleanup() {
+    cd $root_dir
+    rm -rf $source_dir
+    rm -rf $build_dir
+    rm -rf $bin_dir
+}
+
+if [ $1 ]; then
+    $1
+else
+    InstallBuildDependencies
+    GetSources $version
+    BuildProject
+    PackageProject
+    # Cleanup
+fi
