e6f308d3e31b1f9be944ebf47139bdf167cf31bf
Mathieu Comandon <strycore@gmail.com>
2016-10-06 18:24:04 -0700

    [vice] Build runner from svn

--- runners/vice/build.sh
+++ runners/vice/build.sh
@@ -11,6 +11,7 @@ root_dir=$(pwd)
 bin_dir="${root_dir}/${runner_name}"
 arch=$(uname -m)
 version="2.4"
+source_dir="${runner_name}-${version}"
 
 InstallBuildDependencies() {
     deps="libxxf86vm-dev libxmu-dev libxaw7-dev libreadline-dev"
@@ -18,16 +19,15 @@ InstallBuildDependencies() {
 }
 
 GetSources() {
-    src_dir="${runner_name}-${version}"
-    src_archive="${src_dir}.tar.gz"
-    src_url="http://sourceforge.net/projects/vice-emu/files/releases/${src_archive}/download"
-
-    wget ${src_url} -O ${src_archive}
-    tar xzf ${src_archive}
+    #src_archive="${source_dir}.tar.gz"
+    #src_url="http://sourceforge.net/projects/vice-emu/files/releases/${src_archive}/download"
+    #wget ${src_url} -O ${src_archive}
+    #tar xzf ${src_archive}
+    svn checkout svn://svn.code.sf.net/p/vice-emu/code/trunk ${source_dir}
 }
 
 BuildProject() {
-    cd ${src_dir}
+    cd ${source_dir}
     ./configure --prefix=${bin_dir}
     make
     make install
@@ -38,7 +38,10 @@ PackageProject() {
     dest_file="${runner_name}-${version}-${arch}.tar.gz"
     tar czf ${dest_file} ${runner_name}
     runner_upload ${runner_name} ${version} ${arch} ${dest_file}
-    rm -rf ${src_dir} ${src_archive} ${bin_dir}
+}
+
+Cleanup() {
+    rm -rf ${source_dir} ${bin_dir}
 }
 
 if [ $1 ]; then
