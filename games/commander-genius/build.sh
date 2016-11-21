60279ebbacb335afb3d25a81c014a65ef29074de
Rob Loach <robloach@gmail.com>
2016-11-21 02:14:48 -0500

    Update Commander Genius

--- games/commander-genius/build.sh
+++ games/commander-genius/build.sh
@@ -8,14 +8,15 @@ source ${lib_path}util.sh
 source ${lib_path}upload_handler.sh
 
 root_dir=$(pwd)
-version=1.9.0-beta
+version=1.9.6.0-beta
+tag=v1960beta
 arch=$(uname -m)
 source_dir=${root_dir}/commander-genius-src
 
 deps="libsdl2-image-dev libboost-all-dev"
 install_deps $deps
 
-clone https://github.com/gerstrong/Commander-Genius.git $source_dir
+clone https://github.com/gerstrong/Commander-Genius.git $source_dir true $tag
 
 cd $source_dir
 cmake -DUSE_SDL2=yes
