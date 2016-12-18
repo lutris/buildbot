bc9c677240b0bb6be61a4e768434a4afba2506c3
Mathieu Comandon <strycore@gmail.com>
2016-12-17 18:28:44 -0800

    Add libs to Dolphin

--- runners/dolphin/build.sh
+++ runners/dolphin/build.sh
@@ -46,10 +46,14 @@ GetVersion() {
 
 
 PackageProject() {
+    cd ${build_dir}
     rm -rf ${bin_dir}
     mkdir -p ${bin_dir}
     mv Binaries/* ${bin_dir}
     cp -a ${source_dir}/Data/Sys ${bin_dir}
+    mkdir -p ${bin_dir}/lib
+    cp ${build_dir}/Externals/**/*.so* ${bin_dir}/lib
+    cp /usr/lib/x86_64-linux-gnu/libav*-ffmpeg* ${bin_dir}/lib
     cd ${root_dir}
     dest_file="${runner_name}-${version}-${arch}.tar.gz"
     tar czf ${dest_file} ${runner_name}
@@ -72,4 +76,5 @@ else
     BuildProject
     GetVersion
     PackageProject
+    UploadPackage
 fi
