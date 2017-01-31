a76fe2f798e8fe4c6c0676050f64e258bc8835df
Mathieu Comandon <strycore@gmail.com>
2017-01-30 21:32:25 -0800

    [openxcom] Format script

--- games/openxcom/build.sh
+++ games/openxcom/build.sh
@@ -1,11 +1,48 @@
 #!/bin/bash
-SOURCEDIR=OpenXcom-src
-if ! [ -d "$SOURCEDIR" ] ; then
-	git clone https://github.com/SupSuper/OpenXcom.git $SOURCEDIR
+
+set -e
+
+source ../../lib/util.sh
+
+version="0.96.4"
+arch="$(uname -m)"
+root_dir=$(pwd)
+pkg_name="openxcom"
+
+source_dir="${root_dir}/${pkg_name}-src"
+build_dir="${root_dir}/${pkg_name}-build"
+bin_dir="${root_dir}/${pkg_name}"
+
+GetSources() {
+	clone https://github.com/SupSuper/OpenXcom.git $source_dir
+}
+
+InstallBuildDependencies() {
+    install_deps libglade2-dev libsdl-gfx1.2-dev libyaml-cpp-dev
+}
+
+Build() {
+    mkdir -p $build_dir
+    cmake -DCMAKE_INSTALL_PREFIX="${bin_dir}" $source_dir
+    make -j$(getconf _NPROCESSORS_ONLN)
+}
+
+Package() {
+    cd $root_dir
+    tar czf ${pkg_name}-${version}-${arch}.tar.gz ${pkg_name}
+}
+
+CleanUp() {
+    rm -rf $source_dir
+    rm -rf $bin_dir
+}
+
+if [ $1 ]; then
+    $1
+else
+    InstallBuildDependencies
+    GetSources
+    Build
+    Package
+    # CleanUp
 fi
-cd $SOURCEDIR
-mkdir build; cd $_
-cmake ../
-#sudo apt-get install -y libglade2-dev ### FIXME needs actual dependancies
-NUMCPUS=$(($(cat /proc/cpuinfo | grep processor | wc -l) + 1))
-make -j $NUMCPUS
