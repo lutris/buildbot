a560146f40bb05184e19f75741d437d1e1c381e5
Mathieu Comandon <strycore@gmail.com>
2017-03-22 17:38:47 -0700

    [pxsx2] Update build script

--- runners/pcsx2/build.sh
+++ runners/pcsx2/build.sh
@@ -14,29 +14,52 @@ build_dir="${root_dir}/${runner_name}-build"
 bin_dir="${root_dir}/${runner_name}"
 log_file="${root_dir}/${runner_name}.log"
 arch=$(uname -m)
-version="1.4.0"
+version="1.5.0-dev"
 
-deps="wx3.0-headers libaio-dev libasound2-dev libbz2-dev libgl1-mesa-dev \
-    libglu1-mesa-dev libgtk2.0-dev libpng12-dev libpng++-dev libpulse-dev libsdl2-dev \
-    libsoundtouch-dev libwxbase3.0-dev libwxgtk3.0-dev libx11-dev portaudio19-dev zlib1g-dev"
-install_deps $deps
+InstallBuildDependencies() {
+    deps="wx3.0-headers libaio-dev libasound2-dev libbz2-dev libgl1-mesa-dev \
+        libglu1-mesa-dev libgtk2.0-dev libpng12-dev libpng++-dev libpulse-dev libsdl2-dev \
+        libsoundtouch-dev libwxbase3.0-dev libwxgtk3.0-dev libx11-dev portaudio19-dev zlib1g-dev"
+    install_deps $deps
+}
 
-clone https://github.com/PCSX2/pcsx2.git $source_dir
-cd $source_dir
+GetSources() {
+    clone https://github.com/PCSX2/pcsx2.git $source_dir
+}
 
-mkdir -p $build_dir
-cd $build_dir
-cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    ${source_dir} 2>&1 | tee -a $log_file
-make -j$(getconf _NPROCESSORS_ONLN)
-make install
+Build() {
+    mkdir -p $build_dir
+    cd $build_dir
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        ${source_dir} 2>&1 | tee -a $log_file
+    make -j$(getconf _NPROCESSORS_ONLN)
+    make install
+}
 
-rm -rf ${bin_dir}
-mv ${source_dir}/bin ${bin_dir}
+Package() {
+    rm -rf ${bin_dir}
+    mv ${source_dir}/bin ${bin_dir}
+    dest_file="${runner_name}-${version}-${arch}.tar.gz"
+    tar czf ${dest_file} ${runner_name}
+    rm -rf ${build_dir} ${source_dir} ${bin_dir}
+}
 
-dest_file="${runner_name}-${version}-${arch}.tar.gz"
-tar czf ${dest_file} ${runner_name}
-rm -rf ${build_dir} ${source_dir} ${bin_dir}
+Upload() {
+    runner_upload ${runner_name} ${version} ${arch} ${dest_file}
+}
 
-runner_upload ${runner_name} ${version} ${arch} ${dest_file}
+Cleanup() {
+    rm -rf ${source_dir} ${build_dir} ${bin_dir}
+}
+
+if [ $1 ]; then
+    $1
+else
+    InstallBuildDependencies
+    GetSources
+    Build
+    Package
+    Upload
+    Cleanup
+fi
