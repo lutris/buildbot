87d9b280cc99f0ff1f90a2f7abf906b96dcd49e6
Mathieu Comandon <strycore@gmail.com>
2017-04-07 13:46:13 -0700

    [retroarch] Update to 1.5.0

--- runners/libretro/build.sh
+++ runners/libretro/build.sh
@@ -7,7 +7,7 @@ source ${lib_path}util.sh
 source ${lib_path}upload_handler.sh
 
 runner_name=$(get_runner)
-retroarch_version="1.3.6"
+retroarch_version="1.5.0"
 root_dir="$(pwd)"
 source_dir="${root_dir}/libretro-super"
 bin_dir="${root_dir}/retroarch"
