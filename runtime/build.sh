8084758465519d29fb463ee70175529d46f8b530
Mathieu Comandon <strycore@gmail.com>
2017-04-14 09:19:12 -0700

    Remove libsqlite3 from Steam runtime (See #434)

--- runtime/build.sh
+++ runtime/build.sh
@@ -47,6 +47,7 @@ if [ "$STEAM" = '1' ]; then
     rm -rf amd64/usr/lib/x86_64-linux-gnu/libgthread-2.0.so*
     rm -rf amd64/usr/lib/x86_64-linux-gnu/libgomp.so.1*
     rm -rf amd64/usr/lib/x86_64-linux-gnu/libstdc++.so.6*
+    rm -rf amd64/usr/lib/x86_64-linux-gnu/libsqlite3*
     rm -rf amd64/usr/lib/x86_64-linux-gnu/libncurses*
     rm -rf amd64/usr/lib/x86_64-linux-gnu/libtinfo*
     rm -rf amd64/usr/lib/x86_64-linux-gnu/libtdb.so*
@@ -76,6 +77,7 @@ if [ "$STEAM" = '1' ]; then
     rm -rf i386/usr/lib/i386-linux-gnu/libgthread-2.0.so*
     rm -rf i386/usr/lib/i386-linux-gnu/libgomp.so.1*
     rm -rf i386/usr/lib/i386-linux-gnu/libstdc++.so.6*
+    rm -rf i386/usr/lib/i386-linux-gnu/libsqlite3*
     rm -rf i386/usr/lib/i386-linux-gnu/libncurses*
     rm -rf i386/usr/lib/i386-linux-gnu/libtinfo*
     rm -rf i386/usr/lib/i386-linux-gnu/libtdb.so*
