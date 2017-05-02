406e41b0c8a39b4a6ca52a5786fe7aeeb4ed7d3c
Mathieu Comandon <strycore@gmail.com>
2017-05-01 17:02:00 -0700

    Add opentomb config files

---
+++ games/opentomb/autoexec.lua
@@ -0,0 +1,26 @@
+-- LUA autoexec config file
+-- CVAR's section. here you can create and delete CVAR's
+
+if(cvars == nil) then
+    cvars = {};
+end;
+
+-- CVAR's section. here you can create and delete CVAR's
+cvars.show_fps = 1;
+cvars.free_look_speed = 2500;
+
+-- AUTOEXEC LINES
+setLanguage("english");
+
+setGravity(0, 0, -5700.0);
+mlook(1);
+freelook(0);
+cam_distance(1024.0);
+noclip(0);
+setgamef(1, 1);
+--loadMap(base_path .. "data/tr1/LEVEL1.PHD");
+--loadMap(base_path .. "tests/altroom2/LEVEL1.PHD");
+--loadMap(base_path .. "tests/heavy1/LEVEL1.PHD");
+--loadMap(base_path .. "tests/TRIGGERS.PHD");
+--setgamef(1, 14);
+dofile(base_path .. "save/qsave.lua");
