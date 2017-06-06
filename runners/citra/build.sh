918f14abd0829619cfa2667768830462515bf5f5
Mathieu Comandon <strycore@gmail.com>
2017-06-06 07:45:55 -0700

    [citra] Modularize build script

--- runners/citra/build.sh
+++ runners/citra/build.sh
@@ -15,23 +15,48 @@ source_dir=$(pwd)/${runner_name}-src
 build_dir=$(pwd)/${runner_name}-build
 bin_dir=$(pwd)/${runner_name}
 
-deps="libsdl2-dev qtbase5-dev libqt5opengl5-dev gcc-6 g++-6 qt5-default libqt5opengl5-dev xorg-dev lib32stdc++6"
-install_deps $deps
-
-clone https://github.com/citra-emu/citra ${source_dir} recurse
-
-mkdir -p $build_dir
-cmake $source_dir -DCMAKE_BUILD_TYPE=Release
-
-make -j$(getconf _NPROCESSORS_ONLN)
+InstallDependencies() {
+    install_deps libsdl2-dev qtbase5-dev libqt5opengl5-dev gcc-6 g++-6 \
+        qt5-default libqt5opengl5-dev xorg-dev lib32stdc++6 libc++-dev clang
+}
 
 
-mkdir -p $bin_dir
-mv src/citra/citra ${bin_dir}
-mv src/citra_qt/citra-qt ${bin_dir}
+GetSources() {
+    clone https://github.com/citra-emu/citra ${source_dir} recurse
+}
 
-cd ${root_dir}
+Build() {
+    mkdir -p $build_dir
+    cd $build_dir
+    cmake $source_dir \
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_CXX_COMPILER=clang++-3.8 \
+        -DCMAKE_C_COMPILER=clang-3.8 \
+        -DCMAKE_CXX_FLAGS="-O2 -g -stdlib=libc++" \
+    make -j$(getconf _NPROCESSORS_ONLN)
+}
 
-dest_file="${runner_name}-${version}-${arch}.tar.gz"
-tar czf ${dest_file} ${runner_name}
-runner_upload ${runner_name} ${version} ${arch} ${dest_file}
+
+Package() {
+    mkdir -p $bin_dir
+    mv src/citra/citra ${bin_dir}
+    mv src/citra_qt/citra-qt ${bin_dir}
+    cd ${root_dir}
+    dest_file="${runner_name}-${version}-${arch}.tar.gz"
+    tar czf ${dest_file} ${runner_name}
+}
+
+Upload() {
+    runner_upload ${runner_name} ${version} ${arch} ${dest_file}
+}
+
+
+if [ $1 ]; then
+    $1
+else
+    InstallDependencies
+    GetSources
+    Build
+    Package
+    Upload
+fi
