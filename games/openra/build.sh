82ac75221b1e5eca4ad0745afcb6d4b174df8de8
Mathieu Comandon <strycore@gmail.com>
2017-10-08 11:06:13 -0700

    Update OpenRA script

--- games/openra/build.sh
+++ games/openra/build.sh
@@ -4,15 +4,19 @@ set -e
 
 source ../../lib/util.sh
 
-version="20151224"
+version="20170527"
 root_dir=$(pwd)
 source_dir="${root_dir}/openra-src"
+build_dir="${root_dir}/openra-build"
 
 clone https://github.com/OpenRA/OpenRA.git $source_dir
 cd $source_dir
-git checkout release-${version}
+git checkout -b release-${version}
 
 mozroots --import --sync
 make dependencies
 make all
-# mono --debug OpenRA.Game.exe
+make prefix=$build_dir install
+
+cd $build_dir/lib
+tar czf ${root_dir}/openra-${version}.tar.gz openra
