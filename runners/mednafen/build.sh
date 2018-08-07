f7a587c35455daa7214051a6a225e8a3da3d75c0
Mathieu Comandon <strycore@gmail.com>
2018-08-06 19:19:23 -0700

    Update mednafen script

--- runners/mednafen/build.sh
+++ runners/mednafen/build.sh
@@ -12,30 +12,30 @@ root_dir="$(pwd)"
 source_dir="${root_dir}/${runner_name}-src"
 build_dir="${root_dir}/${runner_name}"
 arch="$(uname -m)"
-version="0.9.38.7"
+version="1.21.3"
 
-src_archive="${runner_name}-${version}.tar.bz2"
-src_url="http://mednafen.fobby.net/releases/files/${src_archive}"
+src_archive="${runner_name}-${version}.tar.xz"
+src_url="https://mednafen.github.io/releases/files/${src_archive}"
 
 deps="libsndfile-dev"
 install_deps $deps
 
-wget ${src_url}
-tar xjf ${src_archive}
-rm ${src_archive}
+wget "${src_url}"
+tar xJf "${src_archive}"
+rm "${src_archive}"
 
-mv mednafen ${source_dir}
-cd ${source_dir}
+mv mednafen "${source_dir}"
+cd "${source_dir}"
 
-./configure --prefix=${build_dir}
+./configure --prefix="${build_dir}"
 make
 make install
 
-cd ${build_dir}
+cd "${build_dir}"
 strip bin/mednafen
 
 cd ..
-dest_file=${runner_name}-${version}-${arch}.tar.gz
-tar czf ${dest_file} ${runner_name}
-runner_upload ${runner_name} ${version} ${arch} ${dest_file}
-rm -rf ${build_dir} ${source_dir}
+dest_file="${runner_name}-${version}-${arch}.tar.gz"
+tar czf "${dest_file}" "${runner_name}"
+runner_upload "${runner_name}" "${version}" "${arch}" "${dest_file}"
+rm -rf "${build_dir}" "${source_dir}"
