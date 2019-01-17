fecf4587ecd2cc23669a3297c112bd925c85401b
Mathieu Comandon <strycore@gmail.com>
2019-01-17 15:18:21 -0800

    Set up formatter for console handler

--- runtime/lutrisrt.py
+++ runtime/lutrisrt.py
@@ -7,8 +7,11 @@ import logging
 
 def get_logger():
     logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler = logging.StreamHandler()
     handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
 
