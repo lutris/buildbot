0496b1a9a844cc2fab2e44b7742843473c17a58c
Mathieu Comandon <strycore@gmail.com>
2019-03-19 15:33:01 -0700

    Hatari now uses git

--- runners/hatari/build.sh
+++ runners/hatari/build.sh
@@ -14,12 +14,8 @@ build_dir="${root_dir}/${runner_name}-build"
 bin_dir="${root_dir}/${runner_name}"
 arch=$(uname -m)
 version="2.2.1"
-repo_url="http://hg.tuxfamily.org/mercurialroot/hatari/hatari"
 
-deps="mercurial"
-install_deps $deps
-
-hg clone $repo_url $source_dir
+git clone https://git.tuxfamily.org/hatari/hatari.git/ $source_dir
 
 mkdir -p $build_dir
 cd $build_dir
