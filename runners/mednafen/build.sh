9715c23c8dcda20418ab0d35d15d2976986dee0f
Alexandr <10602045+tannisroot@users.noreply.github.com>
2019-11-05 12:32:07 +0300

    Change archive format to XZ for some runners

--- runners/mednafen/build.sh
+++ runners/mednafen/build.sh
@@ -35,7 +35,7 @@ cd "${build_dir}"
 strip bin/mednafen
 
 cd ..
-dest_file="${runner_name}-${version}-${arch}.tar.gz"
-tar czf "${dest_file}" "${runner_name}"
+dest_file="${runner_name}-${version}-${arch}.tar.xz"
+tar cJf "${dest_file}" "${runner_name}"
 runner_upload "${runner_name}" "${version}" "${arch}" "${dest_file}"
 rm -rf "${build_dir}" "${source_dir}"
