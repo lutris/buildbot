ef0c9990e08724ac113cdf81a37e4772ea8a4478
Alexandr <10602045+tannisroot@users.noreply.github.com>
2019-12-04 03:36:41 +0300

    Use double brackets

--- runners/wine/build.sh
+++ runners/wine/build.sh
@@ -89,12 +89,12 @@ DownloadWine() {
         # The branch name defaults to the build name
         branch_name=${branch_name:-$build_name}
         if [ -d "$source_dir" ]; then
-          if [ `git -C "$source_dir" branch -v | grep -o "$branch_name"` ]; then
+          if [[ `git -C "$source_dir" branch -v | grep -o "$branch_name"` ]]; then
                 git -C "$source_dir" branch -m "$branch_name" "$branch_name"-old
           fi   
 	  git -C "$source_dir" fetch "$repo_url" "$branch_name":"$branch_name"
 	  git -C "$source_dir" checkout "$branch_name"
-          if [ `git -C "$source_dir" branch -v | grep -o "$branch_name"-old` ]; then
+          if [[ `git -C "$source_dir" branch -v | grep -o "$branch_name"-old` ]]; then
                 git -C "$source_dir" branch -D "$branch_name"-old
           fi                   
 	else
