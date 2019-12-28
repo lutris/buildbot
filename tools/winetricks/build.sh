41196104068f5e55494be5af3694bde05d277c09
tannisroot <10602045+tannisroot@users.noreply.github.com>
2019-12-28 03:23:29 +0300

    Improve winetricks script

--- tools/winetricks/build.sh
+++ tools/winetricks/build.sh
@@ -5,30 +5,28 @@ set -e
 source ../../lib/util.sh
 
 pkg_name="winetricks"
-version="master"
 arch=$(uname -m)
 root_dir=$(pwd)
 source_dir="${root_dir}/${pkg_name}-src"
 bin_dir="${root_dir}/${pkg_name}"
+date=$(date '+%Y%m%d')
+dest_file="${pkg_name}-${date}-${arch}.tar.xz"
 
 
 GetSources() {
-    filename="${version}.tar.gz"
-    wget https://github.com/Winetricks/winetricks/archive/${filename}
-    tar xzf ${filename}
-    rm ${filename}
-    mv winetricks-${version} "${source_dir}"
+    if [ -d "$source_dir" ]; then
+      git -C "$source_dir" fetch
+      git -C "$source_dir" reset --hard origin/master
+    else
+      git clone https://github.com/Winetricks/winetricks.git "$source_dir"
+    fi
 }
 
 BuildProject() {
     cd "${source_dir}"
 
-    # Fix language bug on unattended dotnet462 install
-    sed -i 's/WINEDLLOVERRIDES=fusion=b "$WINE" "$file_package" ${W_OPT_UNATTENDED:+$unattended_args}/WINEDLLOVERRIDES=fusion=b "$WINE" "$file_package" \/sfxlang:1027 ${W_OPT_UNATTENDED:+$unattended_args}/g' src/winetricks
-
-    # dotnet471 support, 64-bit mostly working	
-    patch -Np1 < ../'patches/dotnet471.patch'
-    
+    # Apply all patches	
+    for i in ../patches/*.patch; do patch -Np1 < $i; done
     mkdir -p "${bin_dir}"
     cp "${source_dir}/src/winetricks" "${bin_dir}"
 }
@@ -36,21 +34,28 @@ BuildProject() {
 
 PackageProject() {
     cd "$root_dir"
-    tar czf "${pkg_name}-${version}-${arch}.tar.gz" "${pkg_name}"
+    tar cJf "${pkg_name}-${date}-${arch}.tar.xz" "${pkg_name}"
+}
+
+UploadRunner() {
+    cd ${root_dir}
+    aws s3 --endpoint-url=https://nyc3.digitaloceanspaces.com cp ${dest_file} s3://lutris/tools/winetricks/
+    s3cmd setacl s3://lutris/tools/winetricks/${dest_file} --acl-public
+    echo url="https://lutris.nyc3.cdn.digitaloceanspaces.com/tools/winetricks/${dest_file}"
 }
 
 Cleanup() {
     cd "$root_dir"
     rm -rf "$bin_dir"
-    rm -rf "$source_dir"
 }
 
 
 if [ "$1" ]; then
     $1
 else
-    GetSources $version
+    GetSources
     BuildProject
     PackageProject
-    # Cleanup
+    UploadRunner
+    Cleanup
 fi
