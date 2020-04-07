ea99f1cfc31256d027c8533b9837eaec93597782
Mathieu Comandon <strycore@gmail.com>
2020-04-07 00:33:41 -0700

    Fix Citra build on recent Ubuntu versions

--- runners/citra/build.sh
+++ runners/citra/build.sh
@@ -16,7 +16,7 @@ build_dir=$(pwd)/${runner_name}-build
 bin_dir=$(pwd)/${runner_name}
 
 InstallDependencies() {
-    install_deps libsdl2-dev qtbase5-dev libqt5opengl5-dev gcc-6 g++-6 \
+    install_deps libsdl2-dev libfdk-aac-dev qtbase5-dev libqt5opengl5-dev gcc g++ \
         qt5-default libqt5opengl5-dev xorg-dev lib32stdc++6 libc++-dev clang qtmultimedia5-dev
 }
 
@@ -29,18 +29,19 @@ Build() {
     cd "${source_dir}"
     mkdir -p $build_dir
     cd $build_dir
-    cmake $source_dir \
+    cmake \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_CXX_COMPILER=clang++-6.0 \
-        -DCMAKE_C_COMPILER=clang-6.0 \
-        -DCMAKE_CXX_FLAGS="-O2 -g -stdlib=libc++"
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCMAKE_C_COMPILER=clang \
+        -DCMAKE_CXX_FLAGS="-O2 -g" $source_dir
+                                                                    
     make -j$(getconf _NPROCESSORS_ONLN)
 }
 
 
 Package() {
     mkdir -p $bin_dir
-    mv src/citra_qt/citra-qt ${bin_dir}
+    cp bin/* ${bin_dir}
     cd ${root_dir}
     dest_file="${runner_name}-${version}-${arch}.tar.gz"
     tar czf ${dest_file} ${runner_name}
