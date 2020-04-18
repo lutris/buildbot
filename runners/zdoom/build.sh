8acc18d185e45d237b298a015d022a31b1d8bfc5
Mathieu Comandon <strycore@gmail.com>
2020-04-18 12:52:00 -0700

    Remove support for old ass unmaintained decrepit zdoom and imaginary qzdoom no one has heard of

--- runners/zdoom/build.sh
+++ runners/zdoom/build.sh
@@ -14,17 +14,6 @@ source_dir=${root_dir}/${package_name}-src
 build_dir=${root_dir}/${package_name}-build
 bin_dir=${root_dir}/${package_name}
 
-engine_type=zdoom
-
-params=$(getopt -n $0 -o gq --long gzdoom,qzdoom -- "$@")
-eval set -- $params
-while true ; do
-    case "$1" in
-        -g|--gzdoom) engine_type=gzdoom; shift;;
-        -q|--qzdoom) engine_type=qzdoom; shift;;
-        *) shift; break ;;
-    esac
-done
 
 InstallDependencies() {
     install_deps zlib1g-dev libsdl1.2-dev libsdl2-dev libjpeg-dev \
@@ -35,27 +24,17 @@ InstallDependencies() {
 
 GetSources() {
     cd $root_dir
-    if [[ $engine_type == 'zdoom' ]]; then
-        clone https://github.com/rheit/zdoom.git ${source_dir}
-        cd $source_dir
-        version=2.8.1
-        git checkout $version
-    elif [[ $engine_type == 'gzdoom' ]]; then
-        clone https://github.com/coelckers/gzdoom $source_dir
-        cd $source_dir
-        git config --local --add remote.origin.fetch +refs/tags/*:refs/tags/*
-        git pull
-        wget -nc http://zdoom.org/files/fmod/fmodapi44464linux.tar.gz && \
-        tar -xvzf fmodapi44464linux.tar.gz -C ${source_dir}
+    clone https://github.com/coelckers/gzdoom $source_dir
+    cd $source_dir
+    git config --local --add remote.origin.fetch +refs/tags/*:refs/tags/*
+    git pull
+    wget -nc http://zdoom.org/files/fmod/fmodapi44464linux.tar.gz && \
+    tar -xvzf fmodapi44464linux.tar.gz -C ${source_dir}
 
-        version="$(git tag -l | grep -v 9999 | grep -E '^g[0-9]+([.][0-9]+)*$' | \
-                   sed 's/^g//' | sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 | \
-                   tail -n 1 | sed 's/^/g/')"
-        git checkout --detach refs/tags/"$version"
-    elif [[ $engine_type == 'qzdoom' ]]; then
-        echo "qzdoom not supported yet!"
-        exit 2
-    fi
+    version="$(git tag -l | grep -v 9999 | grep -E '^g[0-9]+([.][0-9]+)*$' | \
+                sed 's/^g//' | sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 | \
+                tail -n 1 | sed 's/^/g/')"
+    git checkout --detach refs/tags/"$version"
 }
 
 Build() {
@@ -71,16 +50,14 @@ Build() {
 Package() {
     cd $build_dir
     mkdir -p ${bin_dir}
-    mv $engine_type $engine_type.pk3 ${bin_dir}
-    if [[ $engine_type == 'gzdoom' ]]; then
-        mv lights.pk3 brightmaps.pk3 $bin_dir
-        a='' && [ "$(uname -m)" = x86_64 ] && a=64
-        cp ${source_dir}/fmodapi44464linux/api/lib/libfmodex"$a"-4.44.64.so $bin_dir
-    fi
+    mv gzdoom gzdoom.pk3 ${bin_dir}
+    mv lights.pk3 brightmaps.pk3 $bin_dir
+    a='' && [ "$(uname -m)" = x86_64 ] && a=64
+    cp ${source_dir}/fmodapi44464linux/api/lib/libfmodex"$a"-4.44.64.so $bin_dir
     cd ${bin_dir}
-    strip $engine_type
+    strip gzdoom
     cd ${root_dir}
-    dest_file="${engine_type}-${version}-${arch}.tar.gz"
+    dest_file="gzdoom-${version}-${arch}.tar.gz"
     tar czf ${dest_file} ${package_name}
 }
 
@@ -89,7 +66,7 @@ CleanUp() {
 }
 
 Upload() {
-    runner_upload ${package_name} ${engine_type}-${version} ${arch} ${dest_file}
+    runner_upload ${package_name} gzdoom-${version} ${arch} ${dest_file}
 }
 
 if [ $1 ]; then
