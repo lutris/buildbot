500178f3ad2afd922605b69297f3291430430a39
Thomas Crider <gloriouseggroll@gmail.com>
2020-11-27 02:26:19 -0500

    runners-wine: add gstreamer packages to build.sh

--- runners/wine/build.sh
+++ runners/wine/build.sh
@@ -71,7 +71,10 @@ InstallDependencies() {
         libgnutls28-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev \
         libpng-dev libsdl2-dev libavcodec-dev \
 	    libavutil-dev libswresample-dev libavcodec58 libswresample3 libavutil56 libfaudio0 libfaudio-dev \
-        libvkd3d1 libvkd3d-dev libvkd3d-utils1 libvkd3d-shader1 vkd3d-demos libvulkan1
+        libvkd3d1 libvkd3d-dev libvkd3d-utils1 libvkd3d-shader1 vkd3d-demos libvulkan1 \
+        libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
+        gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x \
+        gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 }
 
 DownloadWine() {
