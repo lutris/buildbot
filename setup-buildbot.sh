25daf2de885a3861b87f0047e756ec8c53ee0bd6
Thomas Crider <gloriouseggroll@gmail.com>
2021-06-27 13:53:14 -0600

    add mingw packages to containers so they can actually use the --usemingw option

--- setup-buildbot.sh
+++ setup-buildbot.sh
@@ -8,7 +8,7 @@ apt -y install software-properties-common
 add-apt-repository ppa:cybermax-dexter/sdl2-backport -y
 add-apt-repository ppa:cybermax-dexter/vkd3d -y
 apt update
-apt -y install wget curl build-essential git python openssh-server s3cmd awscli vim zsh fontconfig sshpass
+apt -y install wget curl build-essential git python openssh-server s3cmd awscli vim zsh fontconfig sshpass gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 g++-mingw-w64-i686 g++-mingw-w64-x86-64
 
 # Install Doctl
 cd ~
