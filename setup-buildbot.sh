a2b054a11294d51cb6e2e3531e1545517d5b3e53
Thomas Crider <gloriouseggroll@gmail.com>
2021-06-26 21:45:12 -0600

    add libjxr-dev to containers to allow building with jxrlib

--- setup-buildbot.sh
+++ setup-buildbot.sh
@@ -8,7 +8,12 @@ apt -y install software-properties-common
 add-apt-repository ppa:cybermax-dexter/sdl2-backport -y
 add-apt-repository ppa:cybermax-dexter/vkd3d -y
 apt update
-apt -y install wget curl build-essential git python openssh-server s3cmd awscli vim zsh fontconfig sshpass
+apt -y install wget curl build-essential git python openssh-server s3cmd awscli vim zsh fontconfig sshpass libjxr-dev
+
+update-alternatives --set x86_64-w64-mingw32-gcc `which x86_64-w64-mingw32-gcc-posix`
+update-alternatives --set x86_64-w64-mingw32-g++ `which x86_64-w64-mingw32-g++-posix`
+update-alternatives --set i686-w64-mingw32-gcc `which i686-w64-mingw32-gcc-posix`
+update-alternatives --set i686-w64-mingw32-g++ `which i686-w64-mingw32-g++-posix`
 
 # Install Doctl
 cd ~
