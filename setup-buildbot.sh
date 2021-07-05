435126d3c7988911ce08dc3f1dba3abb58772732
Thomas Crider <gloriouseggroll@gmail.com>
2021-07-05 12:19:00 -0600

    use cybermax mingw-e64 repo for updated mingw (currently at 9.3)

--- setup-buildbot.sh
+++ setup-buildbot.sh
@@ -5,8 +5,16 @@ set -e
 apt update
 apt -y full-upgrade
 apt -y install software-properties-common
+
+# for sdl2 support
 add-apt-repository ppa:cybermax-dexter/sdl2-backport -y
+
+# for vkd3d support
 add-apt-repository ppa:cybermax-dexter/vkd3d -y
+
+# for latest mingw
+add-apt-repository ppa:cybermax-dexter/mingw-w64-backport -y
+
 apt update
 apt -y install wget curl build-essential git python openssh-server s3cmd awscli vim zsh fontconfig sshpass gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 g++-mingw-w64-i686 g++-mingw-w64-x86-64
 
