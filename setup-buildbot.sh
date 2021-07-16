0e24d5b56bd6803498174372f780485d73804367
Thomas Crider <gloriouseggroll@gmail.com>
2021-07-15 20:37:05 -0600

    fix container prep breakage. woops

--- setup-buildbot.sh
+++ setup-buildbot.sh
@@ -15,16 +15,13 @@ add-apt-repository ppa:cybermax-dexter/vkd3d -y
 # for latest mingw
 add-apt-repository ppa:cybermax-dexter/mingw-w64-backport -y
 
-apt update
-apt -y install wget curl build-essential git python openssh-server s3cmd awscli vim zsh fontconfig sshpass gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 g++-mingw-w64-i686 g++-mingw-w64-x86-64
-
 # for gcc 11
-# since this repo provides other packages besides gcc/g++, we need to add it and install gcc/g++ -after- 
-# build-essential has been installed so that build-essential does not pull other packages from this repository
-# (build essential is a group of build tools)
 add-apt-repository ppa:ubuntu-toolchain-r/test -y
+
 apt update
-apt -y install gcc-11 g++-11
+
+apt -y install wget curl build-essential git python openssh-server s3cmd awscli vim zsh fontconfig sshpass gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 g++-mingw-w64-i686 g++-mingw-w64-x86-64 gcc-11 g++-11
+
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
 
 # Install Doctl
