2041f79ce4b3eb575effef083c948474cb3708c6
tannisroot <10602045+tannisroot@users.noreply.github.com>
2021-07-22 14:30:50 +0300

    Update doctl version

--- setup-buildbot.sh
+++ setup-buildbot.sh
@@ -26,6 +26,6 @@ update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/b
 
 # Install Doctl
 cd ~
-wget https://github.com/digitalocean/doctl/releases/download/v1.60.0/doctl-1.60.0-linux-amd64.tar.gz
-tar xf ~/doctl-1.60.0-linux-amd64.tar.gz
+wget https://github.com/digitalocean/doctl/releases/download/v1.62.0/doctl-1.62.0-linux-amd64.tar.gz
+tar xf ~/doctl-1.62.0-linux-amd64.tar.gz
 sudo mv ~/doctl /usr/local/bin
