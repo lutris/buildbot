782fe0d184239aaa92bf496a88c805efe0b0ab5f
tannisroot <10602045+tannisroot@users.noreply.github.com>
2021-07-22 14:32:18 +0300

    Replace Mingw PPA with mostlyportable-mingw

--- setup-buildbot.sh
+++ setup-buildbot.sh
@@ -12,9 +12,6 @@ add-apt-repository ppa:cybermax-dexter/sdl2-backport -y
 # for vkd3d support
 add-apt-repository ppa:cybermax-dexter/vkd3d -y
 
-# for latest mingw
-add-apt-repository ppa:cybermax-dexter/mingw-w64-backport -y
-
 # for gcc 11
 add-apt-repository ppa:ubuntu-toolchain-r/test -y
 
@@ -23,6 +20,18 @@ apt update
 apt -y install wget curl build-essential git python openssh-server s3cmd awscli vim zsh fontconfig sshpass gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 g++-mingw-w64-i686 g++-mingw-w64-x86-64 gcc-11 g++-11
 
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+
+#Install MinGW
+if [ "$(uname -m)" = "x86_64" ]; then
+    wget https://github.com/lutris/mostlyportable-gcc/releases/download/mingw-11.1.1/mingw-mostlyportable-11.1.1-64.tar.xz
+    tar xf ~/mingw-mostlyportable-11.1.1-64.tar.xz
+    mv ~/mingw-mostlyportable-11.1.1-64 ~/mingw-mostlyportable-11.1.1
+else
+    wget https://github.com/lutris/mostlyportable-gcc/releases/download/mingw-11.1.1/mingw-mostlyportable-11.1.1-32.tar.xz
+    tar xf ~/mingw-mostlyportable-11.1.1-32.tar.xz
+    mv ~/mingw-mostlyportable-11.1.1-32 ~/mingw-mostlyportable-11.1.1
+fi
+sudo sed -i s#'PATH="'#'PATH="/home/ubuntu/mingw-mostlyportable-11.1.1/strip:/home/ubuntu/mingw-mostlyportable-11.1.1/bin:/home/ubuntu/mingw-mostlyportable-11.1.1/lib:'#g /etc/environment
 
 # Install Doctl
 cd ~
