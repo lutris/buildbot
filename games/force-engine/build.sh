7d5c1ec892e357ce4bb83c361e3ceaee7649cf4d
Mathieu Comandon <strycore@gmail.com>
2023-02-09 12:46:25 -0800

    Complete script for The Force Engine:

--- games/force-engine/build.sh
+++ games/force-engine/build.sh
@@ -7,19 +7,19 @@ source ${lib_path}path.sh
 source ${lib_path}util.sh
 source ${lib_path}upload_handler.sh
 
+
 root_dir=$(pwd)
-version=0.7.1
+project="TheForceEngine"
+version=1.08.100
 arch=$(uname -m)
-package_name="TheForceEngine"
 
-source_dir=${root_dir}/${package_name}-src
-build_dir=${root_dir}/${package_name}-build
-bin_dir=${root_dir}/${package_name}
-
+source_dir=${root_dir}/${project}-src
+build_dir=${root_dir}/${project}-build
+bin_dir=${root_dir}/${project}-dist
+build_archive="$project-$version-$arch.tar.xz"
 Deps() {
     deps="libsdl2-dev libdevil-dev librtaudio-dev librtmidi-dev libglew-dev cmake build-essential"
     install_deps $deps
-
 }
 
 Fetch() {
@@ -33,8 +33,23 @@ Build() {
     cmake -S $source_dir -DCMAKE_INSTALL_PREFIX="${bin_dir}"
     make
     make install
+    mv ${bin_dir}/bin/tfelnx ${bin_dir}/share/TheForceEngine
+    mv ${bin_dir}/share/TheForceEngine $root_dir
+
+}
+Package() {
+    cd $root_dir
+    tar cJf $build_archive $project
 }
 
+Upload() {
+    spaces_upload $build_archive "games" "$project"
+}
+
+Clean() {
+    cd $root_dir
+    rm -rf TheForceEngine*
+}
 
 if [ $1 ]; then
     $1
@@ -42,4 +57,6 @@ else
     Deps
     Fetch
     Build
+    Package
+    Upload
 fi
