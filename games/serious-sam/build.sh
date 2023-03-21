db6d373c7025506bca09a6f490e69d4ec084662d
Mathieu Comandon <strycore@gmail.com>
2023-03-20 21:31:27 -0700

    Update Serious Sam script

--- games/serious-sam/build.sh
+++ games/serious-sam/build.sh
@@ -8,23 +8,56 @@ source ${lib_path}util.sh
 source ${lib_path}upload_handler.sh
 
 root_dir=$(pwd)
-package_name='serious-engine'
+package_name='serious-sam'
 arch=$(uname -m)
 source_dir=${root_dir}/${package_name}-src
-bin_dir=${root_dir}/${package_name}
+tfe_bin_dir=${root_dir}/${package_name}-tfe
+tse_bin_dir=${root_dir}/${package_name}-tse
 
-clone https://github.com/rcgordon/Serious-Engine.git $source_dir
+Fetch() {
+    clone https://github.com/tx00100xt/SeriousSamClassic $source_dir
+}
 
-cd $source_dir/Sources
-./build-linux.sh
+BuildTFE() {
+    cd $source_dir/SamTFE/Sources
+    ./build-linux64.sh -DTFE=TRUE
+}
 
-mkdir -p $bin_dir
-mkdir -p $bin_dir/Debug
+BuildTSE() {
+    cd $source_dir/SamTSE/Sources
+    ./build-linux64.sh
+}
 
-cd cmake-build
-mv ecc ssam libShadersD.so $bin_dir
-mv libEntitiesMPD.so $bin_dir/Debug/libEntitiesD.so
-mv libGameMPD.so $bin_dir/Debug/libGameD.so
+PackageTFE() {
+    mkdir -p $tfe_bin_dir
+    cd $source_dir/SamTFE/Bin
+    cp * $tfe_bin_dir
 
-cd $root_dir
-tar cjf ${package_name}-${arch}.tar.bz2 ${package_name}
+    cd $tfe_bin_dir
+    tar cfJ $root_dir/serious-sam-tfe-${arch}.tar.xz *
+}
+
+
+PackageTSE() {
+    mkdir -p $tse_bin_dir
+    cd $source_dir/SamTSE/Bin
+    cp * $tse_bin_dir
+
+    cd $tse_bin_dir
+    tar cfJ $root_dir/serious-sam-tse-${arch}.tar.xz *
+}
+
+Clean() {
+    rm -rf $source_dir
+}
+
+
+if [ $1 ]; then
+    $1
+else
+    Fetch
+    BuildTFE
+    BuildTSE
+    PackageTFE
+    PackageTSE
+fi
