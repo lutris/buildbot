1ca16021ec80f958c92be17d1213bbe50bddb43d
Mathieu Comandon <strycore@gmail.com>
2023-09-19 00:27:35 -0700

    Add libslirp-dev dependency for xemu

--- runners/xemu/build.sh
+++ runners/xemu/build.sh
@@ -15,7 +15,7 @@ source_dir=$(pwd)/${runner_name}-src
 build_dir=$(pwd)/${runner_name}-build
 bin_dir=$(pwd)/${runner_name}
 
-deps="libsdl2-dev libepoxy-dev libpixman-1-dev libgtk-3-dev libssl-dev libsamplerate0-dev libpcap-dev ninja-build python3-yaml"
+deps="libsdl2-dev libslirp-dev libepoxy-dev libpixman-1-dev libgtk-3-dev libssl-dev libsamplerate0-dev libpcap-dev ninja-build python3-yaml"
 install_deps $deps
 
 
