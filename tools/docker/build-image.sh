e78c535c22fdae96c679639f99a096647eeac0aa
GloriousEggroll <gloriouseggroll@gmail.com>
2023-09-20 15:15:35 -0600

    add script for creating future docker container images

---
+++ tools/docker/build-image.sh
@@ -0,0 +1,43 @@
+# This is used for creating new buildbot docker images (for example when we want to change the buildbot container/VM host distro)
+# Currently based off debian 12 bookworm
+#
+# ATTENTION:
+# You must prepare podman to be able to login to docker hub before attempting to push images
+# Create an account then create a token
+# https://hub.docker.com/settings/security?generateToken=true
+# 
+# Then:
+# podman login docker.io
+# Username: <username>
+# Password: <token>
+
+#!/bin/bash
+cat << EOF > imageprep.sh
+#!/bin/bash
+useradd -m vagrant
+mkdir -p /vagrant
+chmod 777 /vagrant
+echo -e 'vagrant\nvagrant\n' | passwd vagrant
+apt-get update && apt-get install -y git sudo
+usermod -aG sudo vagrant
+cd /home/vagrant
+git clone --branch debian12 http://github.com/lutris/buildbot lutris-buildbot
+chown -R vagrant:vagrant /home/vagrant/lutris-buildbot/
+chmod +x /home/vagrant/lutris-buildbot/setup-buildbot.sh
+cd /home/vagrant/lutris-buildbot/
+./setup-buildbot.sh
+EOF
+chmod +x imageprep.sh
+podman pull debian:bookworm
+podman create --interactive --name bookworm debian:bookworm
+podman start bookworm
+podman cp imageprep.sh bookworm:/
+rm imageprep.sh
+podman exec bookworm bash -c "./imageprep.sh"
+podman exec bookworm bash -c "rm imageprep.sh"
+podman stop bookworm
+
+# Change gloriouseggroll/lutris_buildbot:bookworm to your Docker repo and tag
+podman commit bookworm gloriouseggroll/lutris_buildbot:bookworm
+podman push gloriouseggroll/lutris_buildbot:bookworm
+
