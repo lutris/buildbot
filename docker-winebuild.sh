189653cfb8a05a9a7da52191f10fe18cb1c897c4
GloriousEggroll <gloriouseggroll@gmail.com>
2023-09-20 20:42:04 -0600

    add buildbot as mount to docker winebuild script

--- docker-winebuild.sh
+++ docker-winebuild.sh
@@ -8,14 +8,12 @@ if [[ ! -d vagrant_share ]]; then
 	mkdir -p vagrant_share
 fi
 if [[ -z $(podman container list -a | grep buildbot) ]]; then
-	docker create --interactive --name buildbot --mount type=bind,source="$PWD"/vagrant_share,destination=/vagrant,rw=true docker.io/gloriouseggroll/lutris_buildbot:bookworm
+	docker create --interactive --name buildbot --mount type=bind,source="$PWD"/vagrant_share,destination=/vagrant,readonly=false --mount type=bind,source="$PWD",destination=/home/vagrant/buildbot,readonly=false docker.io/gloriouseggroll/lutris_buildbot:bookworm
 fi
 
 docker start buildbot
 
 # cleanup any old builds first
-docker exec buildbot bash -c "cd /home/vagrant/lutris-buildbot && git config --global --add safe.directory /home/vagrant/lutris-buildbot"
-docker exec buildbot bash -c "cd /home/vagrant/lutris-buildbot && git reset --hard HEAD && git clean -xdf && git pull"
 docker exec buildbot bash -c "rm -Rf /home/vagrant/lutris-buildbot/runners/wine/wine-src/"
 
 # start build
